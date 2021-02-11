dab7d0b05aadcb1cfdd6a1619e807079ab339cad
David Ryan Alviola <david.r.alviola@accenturefederal.com>
2021-02-11 15:56:51 -0600

    Updated hypothesis testing exercises and added hypothesis file with helper function

---
+++ hypothesis.py
@@ -0,0 +1,27 @@
+# Helper function to evaluate hypothesis
+def evaluate_hypothesis(null_hypothesis, alternative_hypothesis, alpha, p_value, t_value, tails = "two"):
+    def print_null_hypothesis():
+        print(f"We fail to reject the null hypothesis:  {null_hypothesis}")
+
+    def print_alt_hypothesis():
+        print("We reject the null hypothesis.")
+        print(f"We move forward with the alternative hypothesis:  {alternative_hypothesis}")
+
+    if tails == "two":
+
+        if p_value < alpha:
+            print_alt_hypothesis()
+        else:
+            print_null_hypothesis()
+    else:
+
+        if (p_value / 2) < alpha:
+
+            if (tails == "greater"):
+                if (t_value > 0):
+                    print_alt_hypothesis()
+            else:
+                if (t_value < 0):
+                    print_alt_hypothesis()
+        else:
+            print_null_hypothesis()
